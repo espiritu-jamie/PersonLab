b54114f0043fede726cc1618c71bed00c4671124
Jamie <espiritu_jamie@hotmail.com>
2023-01-23 19:53:19 -0700

    Add project files.

---
+++ PersonLab/Person.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonLab
+{
+    internal class Person
+    {
+        private int personId;
+        private string firstName;
+        private string lastName;
+        private int age;
+        private string favoriteColor;
+        private bool isWorking;
+
+        public Person(int personId, string firstName, string lastName, int age, string favoriteColor, bool isWorking)
+        {
+            this.personId = personId;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.favoriteColor = favoriteColor;
+            this.isWorking = isWorking;
+        }
+
+        public string DisplayPersonInfo()
+        {
+            string firstName = this.firstName;
+            string lastName = this.lastName;
+            int personId = this.personId;
+
+            string name = firstName + " " + lastName;
+
+            string favoriteColor = this.favoriteColor;
+
+            Console.WriteLine(personId + ": " + name + "'s favorite color is " + favoriteColor);
+
+            return personId + name + favoriteColor;
+        }
+
+        public string ChangeFavoriteColor()
+        {
+            string favoriteColor = this.favoriteColor;
+            this.favoriteColor = "White";
+            return favoriteColor;
+        }
+
+        public int GetAgeInTenYears()
+        {
+            string firstName = this.firstName;
+            string lastName = this.lastName;
+            int age = this.age;
+
+            string name = firstName+ " " + lastName;
+
+            int ageInTenYears = age + 10;
+
+            Console.WriteLine(name + "'s Age in 10 years is: " + ageInTenYears);
+
+            return ageInTenYears;
+        }
+
+        public override string ToString()
+        {
+            int personId = this.personId;
+            string firstName = this.firstName;
+            string lastName = this.lastName;
+            int age = this.age;
+            string favoriteColor = this.favoriteColor;
+            bool isWorking = this.isWorking;
+
+            string name = firstName + " " + lastName;
+
+            Console.WriteLine("PersonId: " + personId + "\nFirstName: " + firstName + "\nLastName: " + lastName
+                + "\nFavoriteColor: " + favoriteColor + "\nAge: " + age + "\nIsWorking: " + isWorking);
+
+            return personId + name + favoriteColor + age + isWorking;
+        }
+    }
+}
---
+++ PersonLab/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PersonLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1;
+
+            person1 = new Person(1, "Ian", "Brooks", 30, "Red", true);
+
+            Person person2;
+
+            person2 = new Person(2, "Gina", "James", 18, "Green", false);
+
+            Person person3;
+
+            person3 = new Person(3, "Mike", "Briscoe", 45, "Blue", true);
+
+            Person person4;
+
+            person4 = new Person(4, "Mary", "Beals", 28, "Yellow", true);
+
+
+            string person2Info = person2.DisplayPersonInfo();
+            
+            string person3Info = person3.ToString();
+
+            string person1ChangeColor = person1.ChangeFavoriteColor();
+            string person1Info = person1.DisplayPersonInfo();
+
+            int person4AgeInTenYears = person4.GetAgeInTenYears();
+
+        }
+    }
+}
